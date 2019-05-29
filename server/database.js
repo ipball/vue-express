68b641935675dbc4f07d420129eea48dfeb1d4fe
Tawatsak Tangeaim <tangeaim@hotmail.com>
2019-05-29 14:42:49 +0700

    add static folder

--- server/database.js
+++ server/database.js
@@ -2,16 +2,16 @@ const mongodb = require('mongodb');
 
 async function loadCollection(collect) {
     // #cloud NODEJS
-    // const client = await mongodb.MongoClient.connect
-    // ('mongodb+srv://sandbox:123abc@cluster0-jgzdk.mongodb.net/test?retryWrites=true', {
-    //     useNewUrlParser: true
-    // });
-    
-    // #local NODEJS
     const client = await mongodb.MongoClient.connect
-    ('mongodb://localhost:27017/vue_express', {
+    ('mongodb+srv://sandbox:123abc@cluster0-jgzdk.mongodb.net/test?retryWrites=true', {
         useNewUrlParser: true
     });
+    
+    // #local NODEJS
+    // const client = await mongodb.MongoClient.connect
+    // ('mongodb://localhost:27017/vue_express', {
+    //     useNewUrlParser: true
+    // });
 
     return client.db('vue_express').collection(collect);
 }
