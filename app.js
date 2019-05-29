82bb92e4423c0aaea13ecd515ebbf154b2ae7701
Tawatsak Tangeaim <tangeaim@hotmail.com>
2019-05-29 15:41:29 +0700

    Fx

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
+const port = process.env.PORT || 5000;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(80);
+}).listen(port);
